23c198ac56415863fdc1771619559e65a1a165b5
Paul Nikitin <paulnikitin@gmail.com>
2015-02-07 23:41:38 +0300

    FIX: корректная отдача json

--- core.php
+++ core.php
@@ -1,14 +1,11 @@
 <?php
-/**
- * Ideal CMS (http://idealcms.ru/)
- * @link      http://github.com/ideals/idealcms репозиторий исходного кода
- * @copyright Copyright (c) 2012-2014 Ideal CMS (http://idealcms.ru)
- * @license   http://idealcms.ru/license.html LGPL v3
- */
+header('Content-Type: application/json');
+
 usleep(2000000);
 $rand = rand(1, 10);
 $list = array();
 while ($rand--) {
-    $list[$rand] = rand(0, 100);
+    $list[] = rand(0, 100) . 's';
 }
-echo json_encode($list);
+
+echo json_encode($list, JSON_PRETTY_PRINT);
