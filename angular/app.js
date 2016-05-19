a6a2108579a298ed9ad531ab5220eab5f7160b87
Konstantin Nikitin <knikitin@gmail.com>
2016-05-19 10:31:09 +0300

    Переделал Angular версию на роутинг с ui-router

--- angular/app.js
+++ angular/app.js
@@ -1,44 +1,64 @@
 ( function(){
 
-var app = angular.module('app', []);
- 
-app.controller('mainMenu', [ '$http', '$sce', function($http, $sce) {
-  
-  var mainMenu = this
+var app = angular.module('app', ['ui.router']);
 
-  mainMenu.fullContent = {};
-  mainMenu.sce = $sce;
-  mainMenu.showContent = true;
+app.run(
+      [          '$rootScope', '$state', '$stateParams',
+        function ($rootScope,   $state,   $stateParams) {
+        $rootScope.$state = $state;
+        $rootScope.$stateParams = $stateParams;
+        }
+      ]
+    );
+
+app.config(['$stateProvider', '$urlRouterProvider', '$locationProvider',
+		function($stateProvider, $urlRouterProvider, $locationProvider) {
+
+  $locationProvider.hashPrefix("!");
+
+  $stateProvider
+    .state('mainState', {
+          url: "/{url}",
+          templateUrl: "content.html",
+          controller: ['$scope', '$http', '$sce', '$stateParams', function($scope, $http, $sce, $stateParams){
+
+			var cContest = this
+
+			$scope.fullContent = {};
+			$scope.sce = $sce;
+			$scope.showContent = true;
+
+		    if ($stateParams.url === "/") {
+		      $scope.fullContent = {};
+		      document.location.href='/';
+		    } else {
+		      $scope.showContent = false;
+		      $http.get("/"+$stateParams.url+".json").success(
+		        function(data){
+		          $scope.fullContent = data;
+		          $scope.showContent = true; 
+		        }     
+		      )
+		      .error(function(){
+		         $scope.fullContent = {"header":"Ошибка", "content":"Содержимое раздела не обнаружено."};
+		         $scope.showContent = true; 
+		      });
+		    };
+
+          }]
+    })
+}]);
+
+ 
+app.controller('mainMenu', [ '$http', '$state', function($http, $state) {
+ 
+  var mainMenu = this
 
   mainMenu.listMenu = [];
   $http.get("/menu.json").success(function(data){
       mainMenu.listMenu = data;
-      mainMenu.openUrl(mainMenu.listMenu[1].url);
+      $state.go('mainState', {"url":mainMenu.listMenu[1].url});
     });
-
-  mainMenu.openUrl = function(setCurrent) {
-    mainMenu.currentUrl = setCurrent;
-    if (setCurrent === "/") {
-      mainMenu.fullContent = {};
-      document.location.href='/';
-    } else {
-      mainMenu.showContent = false;
-      $http.get("/"+setCurrent+".json").success(
-        function(data){
-         mainMenu.fullContent = data;
-          mainMenu.showContent = true; 
-        }     
-      )
-      .error(function(){
-         mainMenu.fullContent = {"header":"Ошибка", "content":"Содержимое раздела не обнаружено."};
-         mainMenu.showContent = true; 
-      });
-    };
-  };
-  
-  mainMenu.isActiv = function(testCurrent){
-    return testCurrent === mainMenu.currentUrl;
-  };
 
 }]);
 
